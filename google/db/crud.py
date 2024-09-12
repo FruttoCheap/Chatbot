743b46e965b06ccf632108a6fae08e11484e78b3
giulio.desana <giulio.desana@iakta.it>
2024-09-12 18:22:13 +0200

    crud

--- google/db/crud.py
+++ google/db/crud.py
@@ -4,13 +4,17 @@ from google.db.config import load_config
 
 
 class Expense:
+
     def __init__(self, price, description, category):
         self.price = price
         self.description = description
         self.category = category
 
+    def __repr__(self):
+        return f"Expense(price={self.price}, description={self.description}, category={self.category})"
 
-def insert_vendor(expense: Expense):
+
+def insert_expense(expense: Expense):
     """ Insert a new vendor into the vendors table """
 
     sql = """INSERT INTO expenses(price, description, category)
@@ -35,6 +39,34 @@ def insert_vendor(expense: Expense):
         return expense_id
 
 
+def get_expenses(query: str = None):
+    """ Query expenses from the expenses table """
+
+    if query:
+        sql = query
+    else:
+        sql = """SELECT * FROM expenses"""
+
+    expenses = []
+    config = load_config()
+
+    try:
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql)
+                rows = cur.fetchall()
+
+                for row in rows:
+                    expense = Expense(row[1], row[2], row[3])
+                    expenses.append(expense)
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return expenses
+
+
 if __name__ == '__main__':
-    expense = Expense(100, "Food", "Groceries")
-    insert_vendor(expense)
+    # expense = Expense(100, "some groceries", "Food")
+    # insert_expense(expense)
+    print(get_expenses())
